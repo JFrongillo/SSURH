85b9f2a5af210f28f4722abf904091406a641c3d
Joseph Frongillo <j_frongillo@salemstate.edu>
2023-01-07 18:09:49 -0500

    mc

--- Server/routes/profiles.js
+++ Server/routes/profiles.js
@@ -4,8 +4,8 @@ const userSchema = require("../models/user");
 
 //View one user on the application
 route.get('/:username', async(req,res) =>{
-    const response = await userSchema.find({username: req.params.username})
-    return res.json(response)
+    const response = await userSchema.find({username: req.params.username});
+    return res.json(response);
 })
 
 //View all profiles on the application
@@ -13,6 +13,18 @@ route.get('/', async(req,res) => {
     const response = await userSchema.find();
     return res.json(response);
 })
+
+/** 
+route.put('/:username/edit', async(req,res) => {
+    const user = await userSchema.find({username: req.params.username});
+    if(!user){
+        return res.status(404).send({message: "User not found"})
+    }
+    else{
+        const{firstName, middleName, lastName, username, roomNumber, linkedIn, bio} = req.body;
+
+    }
+})*/
 
 
 
