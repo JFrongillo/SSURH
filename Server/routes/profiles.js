b1a4c8bb8211753953e2f3ffc221ee48049ffc62
JFrongillo <j_frongillo@salemstate.edu>
2023-01-08 08:03:28 -0500

    Edit user info

--- Server/routes/profiles.js
+++ Server/routes/profiles.js
@@ -14,17 +14,35 @@ route.get('/', async(req,res) => {
     return res.json(response);
 })
 
-/** 
 route.put('/:username/edit', async(req,res) => {
-    const user = await userSchema.find({username: req.params.username});
-    if(!user){
-        return res.status(404).send({message: "User not found"})
-    }
-    else{
-        const{firstName, middleName, lastName, username, roomNumber, linkedIn, bio} = req.body;
+    const{firstName, middleName, lastName, roomNumber, linkedIn, bio} = req.body;
 
-    }
-})*/
+    //Lookup the user by the username provided in the path
+    const user = await userSchema.findOne({username: req.params.username});
+
+    //if the user doesnt exist, return 404 and notify the user that the queried user isnt found
+    if(!user){
+        return res.status(404).send({message: "User not Found"})
+    } 
+    
+    //Converts the user document's _id into a String.
+    const userId = JSON.stringify(user._id).replace(/["]+/g, '')
+
+    //Uses the stringified _id to lookup the user and updates their information based on 
+    const response = await userSchema.findByIdAndUpdate(userId, {
+        name:{
+            firstName: firstName,
+            middleName: middleName,
+            lastName: lastName,
+        },
+        roomNumber: roomNumber,
+        linkedIn: linkedIn,
+        bio: bio,
+    })
+
+    return res.json(response)
+    
+})
 
 
 
